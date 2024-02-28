6ad354bb5482ac8fdb470586edf33296798dbc36
Heorhii Markov <hmarkov.dev@gmail.com>
2024-02-28 22:11:41 +0100

    Update add.js

--- controllers/posts/add.js
+++ controllers/posts/add.js
@@ -3,9 +3,19 @@ import { ctrlWrapper } from "../../decorators/index.js";
 
 const add = async (req, res) => {
 	const { _id: owner, firstName, lastName } = req.user;
+	const { header, content } = req.body;
 
-	const result = await Post.create({ ...req.body, owner, firstName, lastName });
-	res.status(201).json(result);
+	const newPost = new Post({
+		header,
+		content,
+		firstName,
+		lastName,
+		owner,
+	});
+	await newPost.save();
+
+	const post = await Post.find();
+	res.status(201).json(post);
 };
 
 export default ctrlWrapper(add);

53da10630e6a0b627cd0f82328aead595a7d14a7
Heorhii Markov <hmarkov.dev@gmail.com>
2024-02-28 21:00:10 +0100

    Update users-schemas.js

--- schemas/users-schemas.js
+++ schemas/users-schemas.js
@@ -1,11 +1,11 @@
 import Joi from "joi";
 
-// const userSignupSchema = Joi.object({
-// 	email: Joi.string().email().required(),
-// 	password: Joi.string().required(),
-// 	firstName: Joi.string().required(),
-// 	lastName: Joi.string().required(),
-// });
+const userSignupSchema = Joi.object({
+	email: Joi.string().email().required(),
+	password: Joi.string().required(),
+	firstName: Joi.string().required(),
+	lastName: Joi.string().required(),
+});
 
 const userSigninSchema = Joi.object({
 	email: Joi.string().email().required(),
